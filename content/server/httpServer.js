218dcbcc40c7c35582c45ecc7015913a4e7b617c
Jan Odvarko <odvarko@gmail.com>
2012-04-25 19:51:02 +0200

    Basic support for Mobile/Fennec monitoring (should be yet cleaned up)

--- content/server/httpServer.js
+++ content/server/httpServer.js
@@ -10,6 +10,12 @@ define([
 function(FBTrace, Options, DefaultPrefs, Events, TabCacheModel) {
 
 // ********************************************************************************************* //
+// Constants
+
+var Cc = Components.classes;
+var Ci = Components.interfaces;
+
+// ********************************************************************************************* //
 // Module
 
 var HttpServer =
@@ -35,16 +41,52 @@ var HttpServer =
         {
             Components.utils.import("resource:///modules/devtools/dbg-server.jsm");
 
-            // Initialize the browser debugger.
-            if (!DebuggerServer.initialized)
+            // RemoteDebugger object should exist on Fennec so, use it if the server
+            // is running on Fennec
+            // https://bugzilla.mozilla.org/show_bug.cgi?id=739966
+            if (typeof(RemoteDebugger) != "undefined")
+            {
+                RemoteDebugger._start();
+                dump("--> httpServer; RemoteDebugger started at: " + RemoteDebugger._getPort());
+            }
+            // Otherwise initialize the browser debugger.
+            else if (!DebuggerServer.initialized)
             {
                 DebuggerServer.init();
-                DebuggerServer.addBrowserActors();
-            }
 
-            // Open a TCP listener
-            // xxxHonza: what about a pref for the port number?
-            DebuggerServer.openListener(2929, false);
+                // xxxHonza: Fennec doesn't have RemoteDebugger yet, so we need to
+                // do yet another check
+                //var appInfo = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULAppInfo);
+                //var fennec = (appInfo.name != "Firefox");
+
+                //dump("--> httpServer; Backend running on " + appInfo.name);
+
+                //var fennec = true;
+                //if (fennec)
+                //    DebuggerServer.addActors("chrome://browser/content/dbg-browser-actors.js");
+                //else
+                //    DebuggerServer.addBrowserActors();
+
+                try
+                {
+                    // Only available on Fennec
+                    DebuggerServer.addActors("chrome://browser/content/dbg-browser-actors.js");
+                }
+                catch (err)
+                {
+                    // This should happen for Firefox
+                    DebuggerServer.addBrowserActors();
+                }
+
+                // Open a TCP listener
+                // xxxHonza: what about a pref for the port number?
+                DebuggerServer.openListener(2929, false);
+                dump("--> httpServer; RemoteDebugger undefined, running on port: 2929");
+            }
+            else
+            {
+                dump("--> httpServer; RemoteDebugger and DebuggerServer already initialized");
+            }
         }
         catch (ex)
         {
